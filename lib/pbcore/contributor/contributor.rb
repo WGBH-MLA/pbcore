b11bed626d5b3b310ff23857cab954a1395c868c
foglabs <foglaboratories@gmail.com>
2019-01-24 13:07:52 -0500

    'add affiliation and portrayal attributes to pbcorecontributor::contributor'

--- lib/pbcore/contributor/contributor.rb
+++ lib/pbcore/contributor/contributor.rb
@@ -3,6 +3,8 @@ require 'pbcore/element'
 module PBCore
   class Contributor::Contributor < Element
     element :contributor, as: :value
+    attribute :affiliation, as: :affiliation
+    attribute :portrayal, as: :portrayal
 
     build_xml do |xml|
       xml.contributor(value, xml_attributes_hash.compact)
