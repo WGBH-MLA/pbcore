b96561d7ef777857779bcf1f8b593316cf69df09
foglabs <foglaboratories@gmail.com>
2019-01-24 14:51:35 -0500

    'portrayal goes on contributorrole'

--- lib/pbcore/contributor/contributor.rb
+++ lib/pbcore/contributor/contributor.rb
@@ -4,7 +4,6 @@ module PBCore
   class Contributor::Contributor < Element
     element :contributor, as: :value
     attribute :affiliation, as: :affiliation
-    attribute :portrayal, as: :portrayal
 
     build_xml do |xml|
       xml.contributor(value, xml_attributes_hash.compact)
