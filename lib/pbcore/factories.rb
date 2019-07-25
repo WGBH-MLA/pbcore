16a035ca0262c1fb410b06042dba5b54f97754a8
foglabs <foglaboratories@gmail.com>
2019-07-25 13:05:16 -0400

    'not just these, but those'

--- lib/pbcore/factories.rb
+++ lib/pbcore/factories.rb
@@ -1,3 +1,5 @@
 GEM_ROOT = File.dirname(File.dirname(File.dirname(__FILE__)))
 
 Dir[File.join(GEM_ROOT, 'spec', 'factories', '*.rb')].each { |file| require(file) }
+Dir[File.join(GEM_ROOT, 'spec', 'factories', 'instantiation', '*.rb')].each { |file| require(file) }
+Dir[File.join(GEM_ROOT, 'spec', 'factories', 'instantiation', 'essence_track', '*.rb')].each { |file| require(file) }
