cead47059e4b999b4f7dc8315ee7d7cc86cd837c
foglabs <foglaboratories@gmail.com>
2019-07-25 12:24:46 -0400

    'like this'

--- lib/pbcore/factories.rb
+++ lib/pbcore/factories.rb
@@ -1,3 +1,5 @@
 GEM_ROOT = File.dirname(File.dirname(File.dirname(__FILE__)))
 
 Dir[File.join(GEM_ROOT, 'spec', 'factories', '*.rb')].each { |file| require(file) }
+
+require_relative '../spec/support/date_time_helpers'
