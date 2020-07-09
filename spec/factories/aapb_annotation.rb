a36589a87584f9ac34c974f9e876f2eee2185fe1
Andrew Myers <andrew_myers@wgbh.org>
2020-07-09 16:37:12 -0400

    Saving aapb annotation factory here. Should move it to AAPB.

---
+++ spec/factories/aapb_annotation.rb
@@ -0,0 +1,52 @@
+require 'pbcore/factories'
+require 'faker'
+
+FactoryBot.define do
+  factory :aapb_pbcore_annotation, class: PBCore::Annotation, parent: :pbcore_annotation do
+    skip_create
+    value { Faker::Quote.famous_last_words }
+    type { Faker::Lorem.word }
+
+    TODO: Move all these traits to a new factory specific to AAPB
+    trait :on_location do
+      type { 'Level of User Access' }
+      value { 'On Location' }
+    end
+
+    trait :online_reading_room do
+      type { 'Level of User Access' }
+      value { 'Online Reading Room' }
+    end
+
+    trait :private do
+      type { 'Level of User Access' }
+      value { 'Private' }
+    end
+
+    trait :organization do
+      value { Faker::TvShows::StarTrek }
+    end
+
+    trait :outside_url do
+      type { 'Outside URL' }
+      value { 'http://www.wgbh.org/' }
+    end
+
+    trait :external_reference_url do
+      type { 'External Reference URL' }
+      value { 'http://www.hbgw.org/' }
+    end
+
+    trait :has_transcript_url do
+      type { 'Transcript URL' }
+      value { 'https://s3.amazonaws.com/americanarchive.org/transcripts/cpb-aacip-111-21ghx7d6/cpb-aacip-111-21ghx7d6-transcript.json' }
+    end
+
+    trait :has_transcript_status do
+      type { 'Transcript Status' }
+      value { 'Correct' }
+    end
+
+    initialize_with { new(attributes) }
+  end
+end
